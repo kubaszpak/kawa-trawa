035587bb73deea06b57e1149aa03c1a1d0a24fcd
Piotr Durniat <piotr@durniat.com>
2021-12-30 12:27:14 +0100

    Setup password hashing

--- server/src/controller/AuthController.ts
+++ server/src/controller/AuthController.ts
@@ -1,20 +1,32 @@
 import { getRepository } from "typeorm";
 import { NextFunction, Request, Response } from "express";
 import { User } from "../entity/User";
+import * as bcrypt from "bcrypt"
 
 export default class AuthController {
 
     private userRepository = getRepository(User);
 
-    async register(request: Request, response: Response, next: NextFunction) {
-        const user = request.body;
 
-        const emailExists = await this.userRepository.findOne({ email: user.email });
-        if (emailExists) {
-            return response.status(400).send({ error: "Email address is already registered" });
+    async hashPassword(password: string) {
+        const salt = await bcrypt.genSalt(10);
+        return bcrypt.hash(password, salt);
+    }
+
+    async register(request: Request, response: Response, next: NextFunction) {
+
+        let user = request.body;
+
+        const emailIsRegistered = await this.userRepository.findOne({ email: user.email });
+        if (emailIsRegistered) {
+            response.status(400).send({ error: "Email address is already registered" });
+            return;
         }
 
-        return this.userRepository.save(user);
+        user.password = await this.hashPassword(user.password);
+
+        this.userRepository.save(user);
+        response.status(200).send(user);
     }
 
     async refresh(request: Request, response: Response, next: NextFunction) {
