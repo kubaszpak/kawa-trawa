6bd8ace06976ce89d838f48bdf1be1832e5dcec2
Piotr Durniat <45066751+piotrdurniat@users.noreply.github.com>
2022-01-27 23:54:18 +0100

    Sort by price (#3)
    * Added product sorting
    
    * Added price sorting ascending and descending

--- web/src/components/ProductsPage.jsx
+++ web/src/components/ProductsPage.jsx
@@ -2,38 +2,106 @@ import React, { useEffect, useCallback, useState } from "react";
 import { ProductsAPI } from "../api/ProductsApi";
 import Grid from "@mui/material/Grid";
 import ProductCard from "./ProductCard";
+import { Typography, Select, MenuItem, Paper, Box } from "@mui/material";
+import { makeStyles } from '@mui/styles';
 
 const ProductsPage = () => {
-	const [products, setProducts] = useState([]);
+    const classes = useStyles();
+    const [products, setProducts] = useState([]);
+    const [sortMode, setSortMode] = useState("price-asc");
 
-	const fetchProducts = async () => {
-		const { data } = await ProductsAPI.getProducts();
-		return data;
-	};
+    const fetchProducts = async () => {
+        const { data } = await ProductsAPI.getProducts();
+        return [...data];
+    };
 
-	const updateShop = useCallback(async () => {
-		const fetchedProducts = await fetchProducts();
-		setProducts(fetchedProducts);
-	}, []);
+    const updateShop = useCallback(async () => {
+        const fetchedProducts = await fetchProducts();
+        setProducts(fetchedProducts);
+    }, []);
 
-	useEffect(() => {
-		updateShop();
-	}, [updateShop]);
+    useEffect(() => {
+        updateShop();
+    }, [updateShop]);
 
-	return (
-		<>
-			<h1>Products page</h1>
-			<Grid container rowSpacing={1} columnSpacing={1}>
-				{products.map((product, index) => (
-					<>
-						<Grid item xs={12} sm={6} md={4} xl={3} key={index}>
-							<ProductCard key={index} product={product} />
-						</Grid>
-					</>
-				))}
-			</Grid>
-		</>
-	);
+    const sortProducts = (property, ascending) => {
+        let newProducts = [...products];
+        if (ascending) {
+            newProducts.sort((a, b) => a[property] - b[property]);
+        } else {
+            newProducts.sort((a, b) => b[property] - a[property]);
+        }
+        setProducts(newProducts);
+    }
+
+    const handleChange = (event) => {
+        const newSortMode = event.target.value;
+
+        switch (newSortMode) {
+            case "price-asc":
+                sortProducts("price", true);
+                break;
+            case "price-desc":
+                sortProducts("price", false);
+                break;
+        }
+
+        setSortMode(newSortMode);
+    };
+
+    return (
+        <>
+            <Box className={classes.justifyCenter}>
+                <Paper className={classes.sortPaper}>
+                    <Box className={classes.alignCenter}>
+                        <Typography variant="body1" display="inline">{"Sortuj wg:\u00A0"}</Typography>
+                        <Select
+                            className={classes.text}
+                            value={sortMode}
+                            label="Age"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value={"price-asc"}>Ceny rosnąco</MenuItem>
+                            <MenuItem value={"price-desc"}>Ceny malejąco</MenuItem>
+                        </Select>
+                    </Box>
+                </Paper>
+            </Box>
+
+            <Box className={classes.productGrid}>
+                <Grid container rowSpacing={2} columnSpacing={2} >
+                    {products.map((product, index) => (
+                        <Grid item xs={12} sm={6} md={4} xl={3} key={index}>
+                            <ProductCard key={index} product={product} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </>
+    );
 };
+
+const useStyles = makeStyles(theme => ({
+    text: {
+        color: "white",
+        borderColor: 'white',
+    },
+    sortPaper: {
+        padding: theme.spacing(1),
+        marginBottom: theme.spacing(1)
+    },
+    alignCenter: {
+        display: "flex",
+        alignItems: "center",
+    },
+    justifyCenter: {
+        display: "flex",
+        justifyContent: "center",
+    },
+    productGrid: {
+        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(2)
+    }
+}));
 
 export default ProductsPage;
