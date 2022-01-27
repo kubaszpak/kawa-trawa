8bef282fc6dc002f3b939dd86bbfa650ae7894ad
kubaszpak <jakub.szpak.it@gmail.com>
2022-01-27 22:32:31 +0100

    Moved ProductsApi.jsx to .js and deleted repetition in ProductsPage

--- web/src/components/ProductsPage.jsx
+++ web/src/components/ProductsPage.jsx
@@ -1,48 +1,39 @@
-import React, { useEffect, useCallback, useState } from 'react';
-import { ProductsAPI } from '../api/ProductsApi';
-import Grid from '@mui/material/Grid';
-import ProductCard from './ProductCard';
+import React, { useEffect, useCallback, useState } from "react";
+import { ProductsAPI } from "../api/ProductsApi";
+import Grid from "@mui/material/Grid";
+import ProductCard from "./ProductCard";
 
 const ProductsPage = () => {
-    const [products, setProducts] = useState([]);
+	const [products, setProducts] = useState([]);
 
-    const fetchProducts = async () => {
-        const { data } = await ProductsAPI.getProducts();
-        return data;
-    }
+	const fetchProducts = async () => {
+		const { data } = await ProductsAPI.getProducts();
+		return data;
+	};
 
-    const updateShop = useCallback(async () => {
-        const fetchedProducts = await fetchProducts();
-        setProducts(fetchedProducts);
-    }, []);
+	const updateShop = useCallback(async () => {
+		const fetchedProducts = await fetchProducts();
+		setProducts(fetchedProducts);
+	}, []);
 
-    useEffect(() => {
-        updateShop();
-    }, [updateShop]);
+	useEffect(() => {
+		updateShop();
+	}, [updateShop]);
 
-    return <>
-        <h1>Products page</h1>
-        <Grid container rowSpacing={1} columnSpacing={1}>
-            {products.map((product, index) =>
-                <>
-                    <Grid item xs={12} sm={6} md={4} xl={3} key={index} >
-                        <ProductCard key={index} product={product} />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} xl={3} key={index} >
-                        <ProductCard key={index} product={product} />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} xl={3} key={index} >
-                        <ProductCard key={index} product={product} />
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={4} xl={3} key={index} >
-                        <ProductCard key={index} product={product} />
-                    </Grid>
-
-                </>
-            )}
-        </Grid>
-
-    </>;
-}
+	return (
+		<>
+			<h1>Products page</h1>
+			<Grid container rowSpacing={1} columnSpacing={1}>
+				{products.map((product, index) => (
+					<>
+						<Grid item xs={12} sm={6} md={4} xl={3} key={index}>
+							<ProductCard key={index} product={product} />
+						</Grid>
+					</>
+				))}
+			</Grid>
+		</>
+	);
+};
 
 export default ProductsPage;
