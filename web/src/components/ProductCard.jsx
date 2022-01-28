e26889dc61a3a8c83cc8f502ef398792b94f095f
Piotr Durniat <piotr@durniat.com>
2022-01-28 01:12:01 +0100

    Product list component

--- web/src/components/ProductCard.jsx
+++ web/src/components/ProductCard.jsx
@@ -5,10 +5,21 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import { useNavigate } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
+import { Box } from '@mui/material';
 
 const ProductCard = ({ product }) => {
+    const navigate = useNavigate();
+    const classes = useStyles();
 
-    return <Card>
+    const showDetails = () => {
+        navigate(`/products/${product.id}`);
+    }
+
+    return <Card
+        sx={{ cursor: "pointer", p: "16px" }}
+    >
         <CardMedia
             component="img"
             image={product.pathToImage}
@@ -18,15 +29,30 @@ const ProductCard = ({ product }) => {
                 {product.name}
             </Typography>
             <Typography>Cena: {product.price}zł</Typography>
-            <Typography variant="body2" color="text.secondary">
-                {product.description}
-            </Typography>
+            <Box className={classes.description}>
+                <Typography variant="body2" color="text.secondary" >
+                    {product.description}
+                </Typography>
+            </Box>
         </CardContent>
         <CardActions>
-            <Button size="small">Dodaj do koszyka</Button>
-            <Button size="small">Szczegóły</Button>
+            <Button size="small" variant="contained" color="secondary">Dodaj do koszyka</Button>
+            <Button size="small" variant="contained" color="secondary" onClick={showDetails}>Szczegóły</Button>
         </CardActions>
-    </Card>
+    </Card >
 };
+
+const useStyles = makeStyles(theme => ({
+    description: {
+        height: 80,
+        textOverflow: "ellipsis",
+        overflow: "hidden",
+        // Addition lines for 4 line ellipsis
+        display: "-webkit-box !important",
+        "-webkitLine-clamp": 4,
+        "-webkit-box-orient": "vertical",
+        whiteSpace: "normal",
+    }
+}));
 
 export default ProductCard;
