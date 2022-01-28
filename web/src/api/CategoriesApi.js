ce9588b963d41daf5df8cfa002e822bf3beb880b
Piotr Durniat <45066751+piotrdurniat@users.noreply.github.com>
2022-01-28 01:12:32 +0100

    Product list component (#4)

---
+++ web/src/api/CategoriesApi.js
@@ -0,0 +1,11 @@
+import API, { authHeader } from "./ApiConnector";
+
+export class CategoriesApi {
+    static getCategory(id) {
+        return API.get(`/categories/${id}`, authHeader());
+    }
+
+    static getCategories() {
+        return API.get("/categories/", authHeader());
+    }
+}
---
+++ web/src/components/ProductsList.jsx
@@ -0,0 +1,92 @@
+
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid";
+import ProductCard from "./ProductCard";
+import { Typography, Select, MenuItem, Paper, Box } from "@mui/material";
+import { makeStyles } from '@mui/styles';
+
+const ProductsList = ({ products, setProducts }) => {
+    const [sortMode, setSortMode] = useState("price-asc");
+    const classes = useStyles();
+
+    const sortProducts = (property, ascending) => {
+        let newProducts = [...products];
+        if (ascending) {
+            newProducts.sort((a, b) => a[property] - b[property]);
+        } else {
+            newProducts.sort((a, b) => b[property] - a[property]);
+        }
+        setProducts(newProducts);
+    }
+
+    const handleChange = (event) => {
+        const newSortMode = event.target.value;
+
+        switch (newSortMode) {
+            case "price-asc":
+                sortProducts("price", true);
+                break;
+            case "price-desc":
+                sortProducts("price", false);
+                break;
+        }
+
+        setSortMode(newSortMode);
+    };
+
+    return (
+        <>
+            <Box className={classes.justifyCenter}>
+                <Paper className={classes.sortPaper}>
+                    <Box className={classes.alignCenter}>
+                        <Typography variant="body1" display="inline">{"Sortuj wg:\u00A0"}</Typography>
+                        <Select
+                            className={classes.text}
+                            value={sortMode}
+                            label="Age"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value={"price-asc"}>Ceny rosnąco</MenuItem>
+                            <MenuItem value={"price-desc"}>Ceny malejąco</MenuItem>
+                        </Select>
+                    </Box>
+                </Paper>
+            </Box>
+
+            <Box className={classes.productGrid}>
+                <Grid container rowSpacing={2} columnSpacing={2} >
+                    {products.map((product, index) => (
+                        <Grid item xs={12} sm={6} md={4} xl={3} key={index}>
+                            <ProductCard key={index} product={product} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </>
+    );
+}
+
+const useStyles = makeStyles(theme => ({
+    text: {
+        color: "white",
+        borderColor: 'white',
+    },
+    sortPaper: {
+        padding: theme.spacing(1),
+        marginBottom: theme.spacing(1)
+    },
+    alignCenter: {
+        display: "flex",
+        alignItems: "center",
+    },
+    justifyCenter: {
+        display: "flex",
+        justifyContent: "center",
+    },
+    productGrid: {
+        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(2)
+    }
+}));
+
+export default ProductsList;
