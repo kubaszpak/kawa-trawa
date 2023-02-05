246a6a24c0ccc118bb0260b12cfe5bbf7c3bf903
kubaszpak <jakub.szpak.it@gmail.com>
2023-02-05 18:31:05 +0100

    More eslint changes

--- web/src/components/CategoryPage.jsx
+++ web/src/components/CategoryPage.jsx
@@ -7,19 +7,24 @@ export default function CategoryPage({ addProductToCart }) {
 	let params = useParams();
 	const [category, setCategory] = useState(null);
 
-	const fetchCategory = async () => {
-		const { data } = await CategoriesApi.getCategory(params.categoryId);
-		return data;
-	};
-
 	const updateCategory = useCallback(async () => {
+		const fetchCategory = async () => {
+			const { data } = await CategoriesApi.getCategory(params.categoryId);
+			return data;
+		};
 		const fetchedCategory = await fetchCategory();
 		setCategory(fetchedCategory);
-	}, []);
+	}, [params.categoryId]);
 
 	useEffect(() => {
 		updateCategory();
 	}, [updateCategory]);
 
-	return <ProductsList addProductToCart={addProductToCart} products={category?.products || []} onDelete={updateCategory} />
+	return (
+		<ProductsList
+			addProductToCart={addProductToCart}
+			products={category?.products || []}
+			onDelete={updateCategory}
+		/>
+	);
 }
